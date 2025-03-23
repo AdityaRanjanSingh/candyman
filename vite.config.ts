1d3d6650594b0f3c85ff14f046ec4d8d24f37a3d
Aditya Ranjan Singh <aditya-ranjan.singh@postnord.com>
2025-03-23 11:16:50 +0100

    Initialize git

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        app: resolve(__dirname, "src", "app"),
+        components: resolve(__dirname, "src", "components"),
+        hooks: resolve(__dirname, "src", "hooks"),
+      },
+    },
+  };
+});
