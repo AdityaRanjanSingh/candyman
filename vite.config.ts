7defe47e502b8c6064a894a8225e05a08d3f73e0
Aditya Ranjan Singh <aditya-ranjan.singh@postnord.com>
2025-03-23 12:03:56 +0100

    Add theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         app: resolve(__dirname, "src", "app"),
